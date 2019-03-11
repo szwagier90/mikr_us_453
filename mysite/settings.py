1587133b981c412b7d714c4986a2f6940acde86d
Paweł Szwagierek <szwagier90@gmail.com>
2019-03-11 15:29:33 +0100

    Extract sensitive data to not versioned file

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+from mysite.local_settings import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,13 +21,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'f5r+egy8e&$mr7b+a4y-5t$cmokmnt#7us(+lo837kzrc#*08e'
+# SECRET_KEY moved to local_settings
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+# ALLOWED_HOSTS moved to local_settings
 
 # Application definition
 
