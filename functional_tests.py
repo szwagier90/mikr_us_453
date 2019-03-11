fc71baf5550ede7b4094ad2f8413deddd17c9825
Paweł Szwagierek <szwagier90@gmail.com>
2019-03-11 14:56:16 +0100

    Unittest

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,17 @@
+import unittest
 from selenium import webdriver
-browser = webdriver.Firefox()
-browser.get('http://127.0.0.1:8000')
-assert 'Django' in browser.title
+
+class ServerPage(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_home_page(self):
+        self.browser.get('http://127.0.0.1:8000')
+        self.assertIn('Django', self.browser.title)
+
+
+if __name__ == '__main__':
+    unittest.main()
