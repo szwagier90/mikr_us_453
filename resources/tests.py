63a4793b283678a564f0e2a7eed94b90d246d18e
Paweł Szwagierek <szwagier90@gmail.com>
2019-03-15 14:03:17 +0100

    [Refactor] Do not test constants in UT - template html
    Do not compare implicitly given html, but render the html from the file

--- resources/tests.py
+++ resources/tests.py
@@ -1,12 +1,14 @@
 from django.test import TestCase
 from django.http import HttpRequest
+from django.template.loader import render_to_string
 
 from resources.views import home_page
 
 class ResourcesPage(TestCase):
     def test_resources_page_returns_correct_html(self):
         request = HttpRequest()
+
         response = home_page(request)
-        self.assertIn('<title>Produkty</title>', response.content.decode('utf-8'))
-        self.assertTrue(response.content.startswith(b'<html>'))
-        self.assertTrue(response.content.strip().endswith(b'</html>'))
+
+        expected_content = render_to_string('home.html')
+        self.assertEqual(response.content.decode(), expected_content)
