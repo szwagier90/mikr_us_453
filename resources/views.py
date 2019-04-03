e912e26a18a42e91453e4f75973a707e4fab2bf4
Paweł Szwagierek <szwagier90@gmail.com>
2019-04-03 14:27:32 +0200

    Show all products on home page

--- resources/views.py
+++ resources/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
 
+from resources.models import Product
+
 def home_page(request):
-	return render(request, 'resources/home.html')
+	all_products = Product.objects.all()
+	return render(request, 'resources/home.html', {'all_products': all_products})
