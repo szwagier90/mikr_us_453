4452792d1840bc702f51201e56f31e29c136d2bd
Paweł Szwagierek <szwagier90@gmail.com>
2019-04-11 01:16:44 +0200

    Do not test constants in UT - template.html

--- mysite/tests.py
+++ mysite/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 
 from django.http import HttpRequest
+from django.template.loader import render_to_string
 from django.urls import resolve
 
 from .views import index
@@ -13,6 +14,6 @@ class ResourcesPage(TestCase):
     def test_resources_page_returns_correct_html(self):
         request = HttpRequest()
         response = index(request)
-        self.assertTrue(response.content.startswith(b'<html>'))
-        self.assertIn('<title>Szwagier Mikrus Server</title>', response.content.decode('utf-8'))
-        self.assertTrue(response.content.endswith(b'</html>'))
+
+        expected_content = render_to_string('mysite/index.html')
+        self.assertEqual(response.content.decode(), expected_content)
