baeb7895daf27426701c10f50189ed0e265e795a
Paweł Szwagierek <szwagier90@gmail.com>
2019-05-09 00:43:55 +0200

    Functional test derive use LiveServerTestCase for isolation

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,28 +1,23 @@
-import unittest
+from django.test import LiveServerTestCase
 
 from selenium import webdriver
 from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.firefox.options import Options
 
-class ServerPage(unittest.TestCase):
+class ServerPage(LiveServerTestCase):
     def setUp(self):
         firefox_options = Options()
         firefox_options.add_argument("--headless")
         self.driver = webdriver.Firefox(options=firefox_options)
-        self.localServer = 'http://127.0.0.1:8000'
 
     def tearDown(self):
         self.driver.quit()
 
     def test_home_page(self):
-        self.driver.get(self.localServer)
+        self.driver.get(self.live_server_url)
         self.assertIn('Szwagier Mikrus Server', self.driver.title)
 
         home_header = self.driver.find_element_by_tag_name("h1").text
         self.assertEqual("Szwagier Mikrus Server", home_header)
 
         self.fail('Finish the test!')
-
-
-if __name__ == '__main__':
-    unittest.main()
