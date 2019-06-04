602dfe833d3d5157fc1e7e1828f9b824ee908b44
Paweł Szwagierek <szwagier90@gmail.com>
2019-06-04 13:02:00 +0200

    Product quantity and number of pieces

--- resources/models.py
+++ resources/models.py
@@ -2,3 +2,5 @@ from django.db import models
 
 class Product(models.Model):
     name = models.TextField(default='')
+    quantity = models.TextField(default='')
+    pieces = models.IntegerField(default=1)
--- resources/views.py
+++ resources/views.py
@@ -4,7 +4,11 @@ from .models import Product
 
 def home_page(request):
     if request.method == 'POST':
-        Product.objects.create(name=request.POST.get('product', ''))
+        Product.objects.create(
+            name=request.POST.get('product', ''),
+            quantity=request.POST.get('quantity', ''),
+            pieces=request.POST.get('pieces', 1),
+        )
         return redirect('/resources/')
 
     products = Product.objects.all()
