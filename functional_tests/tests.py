45afd5ca4b7c797750cf5cd78106b8703d4e8b47
Paweł Szwagierek <szwagier90@gmail.com>
2022-12-20 02:34:39 +0100

    Tasks link visible on home_page

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -20,4 +20,21 @@ class ServerPage(LiveServerTestCase):
         home_header = self.driver.find_element_by_tag_name("h1").text
         self.assertEqual("Szwagier Mikrus Server", home_header)
 
+
+class TasksPage(LiveServerTestCase):
+    def setUp(self):
+        firefox_options = Options()
+        firefox_options.add_argument("--headless")
+        self.driver = webdriver.Firefox(options=firefox_options)
+
+    def tearDown(self):
+        self.driver.quit()
+
+    def test_home_page(self):
+        self.driver.get(self.live_server_url)
+        self.assertIn('Szwagier Mikrus Server', self.driver.title)
+
+        tasks_page_link = self.driver.find_element_by_id("id_tasks_home")
+        self.assertEqual("Zadania", tasks_page_link.text)
+
         self.fail('Finish the test!')
