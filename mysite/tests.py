2ccb55cb1ef5b380ee81db2e34ff210894b33a1b
Paweł Szwagierek <szwagier90@gmail.com>
2023-01-31 01:20:37 +0100

    Use client object, not request+response

--- mysite/tests.py
+++ mysite/tests.py
@@ -1,7 +1,6 @@
 from django.test import TestCase
 
 from django.urls import resolve
-from django.http import HttpRequest
 from django.template.loader import render_to_string
 
 from .views import index
@@ -9,9 +8,7 @@ from .views import index
 class ServerPage(TestCase):
 
     def test_index_returns_correct_html(self):
-        request = HttpRequest()
-
-        response = index(request)
+        response = self.client.get('/')
         html = response.content.decode('utf8')
 
         expected_content = render_to_string('mysite/index.html')
